0a306044ba519c99b71fadeaac5cbb2a1b37a43d
SakuraiTsubaki <nokang3@naver.com>
2026-09-23 23:21:40 +0900

    Add 16-bit move and item core loaders

--- asm/expansion/item16.asm
+++ asm/expansion/item16.asm
@@ -1,4 +1,6 @@
-; YELLOW 16-bit item table resolver.
+; YELLOW 16-bit item table resolver and core loader.
+
+INCLUDE "asm/expansion/item_core.inc"
 
 YellowResolveItemRecord16::
 ; Input: BC = logical item ID
@@ -6,3 +8,15 @@ YellowResolveItemRecord16::
 ; Carry set if no item record exists.
     ld hl, YellowItemTableDescriptor
     jp YellowResolveTable16
+
+YellowCopyItemCore16Locked::
+; Input: BC = logical item ID, HL = WRAM destination.
+; PRECONDITION: interrupts disabled.
+    push hl
+    call YellowResolveItemRecord16
+    pop hl
+    ret c
+    ld a, YELLOW_ITEM_CORE_V1_SIZE
+    call YellowCopyFromBank9Locked
+    and a
+    ret
--- asm/expansion/move16.asm
+++ asm/expansion/move16.asm
@@ -1,4 +1,6 @@
-; YELLOW 16-bit move table resolver.
+; YELLOW 16-bit move table resolver and core loader.
+
+INCLUDE "asm/expansion/move_core.inc"
 
 YellowResolveMoveRecord16::
 ; Input: BC = logical move ID
@@ -6,3 +8,15 @@ YellowResolveMoveRecord16::
 ; Carry set if no move record exists.
     ld hl, YellowMoveTableDescriptor
     jp YellowResolveTable16
+
+YellowCopyMoveCore16Locked::
+; Input: BC = logical move ID, HL = WRAM destination.
+; PRECONDITION: interrupts disabled.
+    push hl
+    call YellowResolveMoveRecord16
+    pop hl
+    ret c
+    ld a, YELLOW_MOVE_CORE_V1_SIZE
+    call YellowCopyFromBank9Locked
+    and a
+    ret
