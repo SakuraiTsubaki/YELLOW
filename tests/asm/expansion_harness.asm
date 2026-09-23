81b86eb080549fb36f35598d878f4cece25258d2
SakuraiTsubaki <nokang3@naver.com>
2026-09-23 23:22:05 +0900

    Exercise species move item core loaders

--- tests/asm/expansion_harness.asm
+++ tests/asm/expansion_harness.asm
@@ -29,13 +29,15 @@ HarnessHighSpecies::
     yellow_species_core_v1 100, 120, 90, 110, 130, 95, 110, 45, $0102, $0103, 320, 4, 127, 50, 40, 1
 HarnessHighSpeciesEnd::
 
-SECTION "Harness High Data", ROMX[$4100], BANK[$100]
-HarnessHighData::
-    db $21, $22, $23, $24
+SECTION "Harness High Move", ROMX[$4100], BANK[$100]
+HarnessHighMove::
+    yellow_move_core_v1 $0102, $0201, 95, 100, 10, 0, 1, 0, 20, $00000001
+HarnessHighMoveEnd::
 
-SECTION "Harness Top Data", ROMX[$7ffc], BANK[$1ff]
-HarnessTopData::
-    db $31, $32, $33, $34
+SECTION "Harness Top Item", ROMX[$7ff0], BANK[$1ff]
+HarnessTopItem::
+    yellow_item_core_v1 120000, 1, 0, $0123, $0456, 80, 1, $0200, $0001
+HarnessTopItemEnd::
 
 SECTION "Harness Species Index", ROMX[$5000], BANK[$02]
 YellowSpeciesIndex16::
@@ -45,13 +47,13 @@ YellowSpeciesIndex16End::
 
 SECTION "Harness Move Index", ROMX[$5100], BANK[$02]
 YellowMoveIndex16::
-    yellow_farptr9 HarnessHighData
+    yellow_farptr9 HarnessHighMove
     yellow_farptr9 HarnessLowSpecies
 YellowMoveIndex16End::
 
 SECTION "Harness Item Index", ROMX[$5200], BANK[$02]
 YellowItemIndex16::
-    yellow_farptr9 HarnessTopData
+    yellow_farptr9 HarnessTopItem
     yellow_farptr9 HarnessLowSpecies
 YellowItemIndex16End::
 
@@ -66,22 +68,14 @@ HarnessDescriptorsEnd::
 
 ASSERT HarnessLowSpeciesEnd - HarnessLowSpecies == YELLOW_SPECIES_CORE_V1_SIZE
 ASSERT HarnessHighSpeciesEnd - HarnessHighSpecies == YELLOW_SPECIES_CORE_V1_SIZE
+ASSERT HarnessHighMoveEnd - HarnessHighMove == YELLOW_MOVE_CORE_V1_SIZE
+ASSERT HarnessTopItemEnd - HarnessTopItem == YELLOW_ITEM_CORE_V1_SIZE
 ASSERT YellowSpeciesIndex16End - YellowSpeciesIndex16 == 6
 ASSERT YellowMoveIndex16End - YellowMoveIndex16 == 6
 ASSERT YellowItemIndex16End - YellowItemIndex16 == 6
 ASSERT HarnessDescriptorsEnd - YellowSpeciesTableDescriptor == 18
 
 SECTION "Harness Calls", ROM0
-HarnessCopyHighData::
-    ld bc, BANK(HarnessHighData)
-    ld de, HarnessHighData
-    ld hl, wHarnessBuffer
-    ld a, 4
-    di
-    call YellowCopyFromBank9Locked
-    ei
-    ret
-
 HarnessLoadHighSpeciesCore::
     ld bc, 1
     ld hl, wHarnessBuffer
@@ -90,24 +84,18 @@ HarnessLoadHighSpeciesCore::
     ei
     ret
 
-HarnessResolveMove1::
-    ld bc, 1
-    call YellowResolveMoveRecord16
-    ret c
+HarnessLoadHighMoveCore::
+    ld bc, 0
     ld hl, wHarnessBuffer
-    ld a, 4
     di
-    call YellowCopyFromBank9Locked
+    call YellowCopyMoveCore16Locked
     ei
     ret
 
-HarnessResolveItem0::
+HarnessLoadTopItemCore::
     ld bc, 0
-    call YellowResolveItemRecord16
-    ret c
     ld hl, wHarnessBuffer
-    ld a, 4
     di
-    call YellowCopyFromBank9Locked
+    call YellowCopyItemCore16Locked
     ei
     ret
